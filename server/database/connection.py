b43674043da624003b6db28e697769931fe075df
Gwenn01 <arnelgwenn@gmail.com>
2026-01-13 12:29:34 +0800

    setup database

--- server/database/connection.py
+++ server/database/connection.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="citizen_feedback_db"
+        host="bzenxtewg8yce5bnidpx-mysql.services.clever-cloud.com",
+        user="ucayuca9rs952tf9",
+        password="rO8hOxk7JmLQgq1jth6G",
+        database="bzenxtewg8yce5bnidpx"
     )
