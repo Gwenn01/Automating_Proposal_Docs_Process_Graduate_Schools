5586d4d90c2bf95f393c2234d159251d1d52a1c1
Gwenn01 <arnelgwenn@gmail.com>
2026-01-20 13:00:41 +0800

    done creating cover page mapping

--- server/controller/mapper/proposal_mapper.py
+++ server/controller/mapper/proposal_mapper.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from textwrap import dedent
+
 def implementor_view_proposal_mapper(fetch_user_proposals):
     if not fetch_user_proposals:
         return {}
@@ -17,50 +20,62 @@ def implementor_view_proposal_mapper(fetch_user_proposals):
 
     return data
 
-from datetime import datetime
 
 def view_cover_page_mapper(cover):
+    cover = cover[0]
     submission_date = cover["submission_date"].strftime("%B %d, %Y")
 
-    return f"""
-        {submission_date}
+    return dedent(f"""
+    {submission_date}
 
-        DR. ROY N. VILLALOBOS
-        University President
-        President Ramon Magsaysay State University
+    DR. ROY N. VILLALOBOS
+    University President
+    President Ramon Magsaysay State University
 
-        Dear Sir:
+    Dear Sir:
 
-        I have the honor to submit the proposal for your consideration and appropriate action for the proposed extension program entitled {cover["board_resolution_title"]}, with the approved budget of {cover["approved_budget_words"]} (Php {cover["approved_budget_amount"]:,.2f}), with the duration of {cover["duration_words"]} ({cover["duration_years"]}) year(s), covering the period {cover["date_from_to"]}. (please refer to the approved Board Resolution No. {cover["board_resolution_no"]})
+    I have the honor to submit the proposal for your consideration and appropriate action for the
+    proposed extension program entitled {cover["board_resolution_title"]}, with the approved budget
+    of {cover["approved_budget_words"]} (Php {cover["approved_budget_amount"]:,.2f}), with the duration
+    of {cover["duration_words"]} ({cover["duration_years"]}) year(s), covering the period
+    {cover["date_from_to"]}, as approved under {cover["board_resolution_no"]}.
 
-        This program includes an activity entitled {cover["activity_title"]}, to be conducted on {cover["activity_date"]} at {cover["activity_venue"]}. This activity is valuable as it aims to {cover["activity_value_statement"]}. The requested expenses for this activity from the university amount to Php {cover["requested_activity_budget"]:,.2f}, which will be used to defray expenses for food, transportation, supplies and materials, and other expenses related to the activity.
+    This program includes an activity entitled {cover["activity_title"]}, to be conducted on
+    {cover["activity_date"]} at {cover["activity_venue"]}. This activity is valuable as it aims to
+    {cover["activity_value_statement"]}. The requested expenses for this activity from the university
+    amount to Php {cover["requested_activity_budget"]:,.2f}, which will be used to defray expenses for
+    food, transportation, supplies and materials, and other expenses related to the activity.
 
-        Further, there will be {cover["prmsu_participants_words"]} ({cover["prmsu_participants_num"]}) participants from PRMSU, {cover["partner_agency_participants_words"]} ({cover["partner_agency_participants_num"]}) participants from the partner agency, {cover["partner_agency_name"]}, and {cover["trainees_words"]} ({cover["trainees_num"]}) trainees from the abovementioned community.
+    Further, there will be {cover["prmsu_participants_words"]} ({cover["prmsu_participants_num"]})
+    participants from PRMSU, {cover["partner_agency_participants_words"]}
+    ({cover["partner_agency_participants_num"]}) participants from the collaborating agency,
+    {cover["partner_agency_name"]}, and {cover["trainees_words"]} ({cover["trainees_num"]}) trainees
+    from the abovementioned community.
 
-        Your favorable response regarding this matter will be highly appreciated.
+    Your favorable response regarding this matter will be highly appreciated.
 
-        Prepared by:
+    Prepared by:
 
-        __________________________
-        Proponent
+    __________________________
+    Proponent
 
-        Noted by:
+    Noted by:
 
-        __________________________
-        College Dean
+    __________________________
+    College Dean
 
-        Endorsed by:
+    Endorsed by:
 
-        KATHERINE M. UY, MAEd
-        Director, Extension Services
+    KATHERINE M. UY, MAEd
+    Director, Extension Services
 
-        Recommending Approval:                        Certified Funds Available:
+    Recommending Approval:                        Certified Funds Available:
 
-        MARLON JAMES A. DEDICATORIA, Ph.D.            ROBERTO C. BRIONES JR., CPA
-        Vice-President, Research and Development     University Accountant IV
+    MARLON JAMES A. DEDICATORIA, Ph.D.            ROBERTO C. BRIONES JR., CPA
+    Vice-President, Research and Development     University Accountant IV
 
-        Approved by:
+    Approved by:
 
-        ROY N. VILLALOBOS, DPA
-        University President
-        """.strip()
+    ROY N. VILLALOBOS, DPA
+    University President
+    """).strip()
--- server/controller/test_run.py
+++ server/controller/test_run.py
@@ -3,10 +3,10 @@ from model.general.get_proposal import (
     fetch_proposal_cover_page,
     fetch_proposal_content
 )
-from controller.mapper.proposal_mapper import implementor_view_proposal
+from controller.mapper.proposal_mapper import implementor_view_proposal_mapper, view_cover_page_mapper
 
 def run_tests_controller():
     # data = fetch_user_proposals(7)
     # print(implementor_view_proposal(data))
-    print(fetch_proposal_cover_page(24))
+    print(view_cover_page_mapper(fetch_proposal_cover_page(24)))
     print("Running test controller..")
