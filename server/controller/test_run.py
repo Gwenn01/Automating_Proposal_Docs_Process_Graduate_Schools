85b32677f8e84b8f424b225f5e062874fbea384e
Gwenn01 <arnelgwenn@gmail.com>
2026-01-20 12:01:47 +0800

    create a mapper for view proposal

--- server/controller/test_run.py
+++ server/controller/test_run.py
@@ -1,3 +1,12 @@
+from model.general.get_proposal import (
+    fetch_user_proposals,
+    fetch_proposal_cover_page,
+    fetch_proposal_content
+)
+from controller.mapper.proposal_mapper import implementor_view_proposal
 
 def run_tests_controller():
+    # data = fetch_user_proposals(7)
+    # print(implementor_view_proposal(data))
+    print(fetch_proposal_cover_page(24))
     print("Running test controller..")
