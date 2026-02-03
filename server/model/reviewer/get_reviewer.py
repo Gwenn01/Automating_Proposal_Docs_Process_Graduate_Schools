e986ecd96119176c92b8ff20011f3fb3717d6ebb
Gwenn01 <arnelgwenn@gmail.com>
2026-02-03 14:03:07 +0800

    fix the error

--- server/model/reviewer/get_reviewer.py
+++ server/model/reviewer/get_reviewer.py
@@ -18,24 +18,28 @@ def get_reviewer_id(proposal_id):
         return None
 
 def get_reviewer_per_docs(proposal_id, reviewer_id):
-    conn = get_db_connection()
-    cur = conn.cursor(dictionary=True)
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor(dictionary=True)
 
-    query = """
-        SELECT 
-            pr.proposal_id,
-            pr.user_id,
-            pr.review_deadline
-            u.fullname,
-            u.email
-        FROM proposal_reviews pr
-        LEFT JOIN users u ON pr.user_id = u.user_id
-        WHERE pr.proposal_id = %s AND pr.user_id != %s
-    """
+        query = """
+            SELECT 
+                pr.proposal_id,
+                pr.user_id,
+                pr.review_deadline,
+                u.fullname,
+                u.email
+            FROM proposal_reviews pr
+            LEFT JOIN users u ON pr.user_id = u.user_id
+            WHERE pr.proposal_id = %s AND pr.user_id != %s
+        """
 
-    cur.execute(query, (proposal_id, reviewer_id))
-    reviewer = cur.fetchall()
+        cur.execute(query, (proposal_id, reviewer_id))
+        reviewer = cur.fetchall()
 
-    cur.close()
-    conn.close()
-    return reviewer
+        cur.close()
+        conn.close()
+        return reviewer
+    except Exception as e:
+        print(e)
+        return False
