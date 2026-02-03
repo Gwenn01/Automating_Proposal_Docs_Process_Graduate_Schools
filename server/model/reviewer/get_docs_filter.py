636a4358e2d9f95ac9b4c1c47a198188678f76d1
Gwenn01 <arnelgwenn@gmail.com>
2026-02-03 15:39:28 +0800

    save changes

---
+++ server/model/reviewer/get_docs_filter.py
@@ -0,0 +1,10 @@
+from utils.execute_query import execute_query
+
+def get_completed_docs_db():
+    try:
+        query = """
+
+        """
+    except Exception as e:
+        print(e)
+        return None
